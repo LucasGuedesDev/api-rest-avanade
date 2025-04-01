1a322a118bf84d2c3bb5bb197959ccf25d314d22
LucasGuedesDev <guedeslucas033@gmail.com>
2025-03-31 23:21:48 -0300

    classe

--- src/main/java/com/example/api_rest/ApiRestApplication.java
+++ src/main/java/com/example/api_rest/ApiRestApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiRestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
 	}
-
 }
 
